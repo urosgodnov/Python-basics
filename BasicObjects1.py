893fe0c81b46d6969758604554e074c7ce3997f2
uros.godnov@gmail.com <uros.godnov@gmail.com>
2018-05-18 18:41:01 +0200

    adding

---
+++ BasicObjects1.py
@@ -0,0 +1,52 @@
+################Lists
+
+# Assign a list to an variable named my_list
+my_list = [1,2,3]
+
+my_list = ['A string',23,100.232,'o']
+
+# Length of object
+len(my_list)
+
+# Indexing and Slicing
+
+my_list = ['one','two','three',4,5]
+
+# Grab element at index 0
+my_list[0]
+
+# Grab index 1 and everything past it
+my_list[1:]
+
+# Grab everything UP TO index 3
+my_list[:3]
+
+
+# Reassign
+my_list = my_list + ['add new item permanently']
+
+my_list
+
+# Make the list double
+my_list=my_list * 2
+
+my_list
+
+# Append
+list1 = [1,2,3]
+
+list1.append('append me!')
+
+list1
+
+# Pop off the 0 indexed item
+list1.pop(0)
+
+list1
+
+# Use reverse to reverse order (this is permanent!)
+list1.reverse()
+
+list1
+# Sorting
+new_list.sort()
